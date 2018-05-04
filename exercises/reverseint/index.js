79ddcd19de9aa2e1ad8f642396a929dec1b52657
B Terbush <brittanyterbush@gmail.com>
2018-05-04 15:48:27 -0700

    first commit

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,9 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
-
+function reverseInt(n) {
+// use parseInt(string), .toString(), and Math.sign(x)
+  const reversed = n.toString().split('').reverse().join('')
+  return parseInt(reversed) * Math.sign(n)
+}
 module.exports = reverseInt;
--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,13 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
-
+function reverse(str) {
+  let reversed = '';
+  debugger;
+  for (let char of str) {
+    reversed = char + reversed
+  }
+  return reversed;
+}
+reverse('asdf')
 module.exports = reverse;
